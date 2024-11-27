b2f387c277da0301244afae09b85b79ad304b287
Ederson Silva <edsjcbra@gmail.com>
2024-11-27 14:16:29 +0000

    Initial Commit

---
+++ src/main/java/com/tech/EcomApi/Demo/EcomApiDemo/EcomApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tech.EcomApi.Demo.EcomApiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApiDemoApplication.class, args);
+	}
+
+}
